938d295abd3051569a00c6fbf26eb266ae581525
b1dal <125753570+b1dal@users.noreply.github.com>
2024-10-25 10:41:09 -0400

    added main.php and style.css
    building from figma mockup

---
+++ main.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opus</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYPo5SH9y0gw7jDauvpimHhAjoWuUhzpUoYg&s" class="logo">
+        <div class="titleContainer">
+            <h1>Opus</h1>
+            <h3>Work Program Management</h3>
+        </div>
+    </header>
+    <div class="menuBar">
+        <!-- put navbar here -->
+    </div>
+</body>
+</html>
