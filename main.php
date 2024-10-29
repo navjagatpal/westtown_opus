d8884e230e2f22a1d58483d587693683842c66ae
b1dal <125753570+b1dal@users.noreply.github.com>
2024-10-29 09:54:22 -0400

    added navigation bar

--- main.php
+++ main.php
@@ -8,14 +8,19 @@
 </head>
 <body>
     <header class="header">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYPo5SH9y0gw7jDauvpimHhAjoWuUhzpUoYg&s" class="logo">
+        <img src="https://www.westtown.edu/wp-content/uploads/2023/04/cropped-android-chrome-512x512-1.png" class="logo">
         <div class="titleContainer">
             <h1>Opus</h1>
             <h3>Work Program Management</h3>
         </div>
     </header>
     <div class="menuBar">
-        <!-- put navbar here -->
+        <ul>
+            <li><a href="  <put page links here>  ">Test</a></li>
+            <li><a href="  <put page links here>  ">Test</a></li>
+            <li><a href="  <put page links here>  ">Test</a></li>
+            <li><a href="  <put page links here>  ">Test</a></li>
+        </ul> 
     </div>
 </body>
 </html>
