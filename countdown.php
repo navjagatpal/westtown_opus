824f22bba89a0d8124799c253b5cab2613239939
b1dal <125753570+b1dal@users.noreply.github.com>
2024-10-29 10:07:09 -0400

    created different pages and moved header/navbar to header.php
    created 3 new pages that are linked on the navbar (request to switch, double-t, countdown)
    
    moved header/navbar to header.php and used php include to unify across pages

--- countdown.php
+++ countdown.php
@@ -6,7 +6,9 @@
     <title>Opus</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <?php include('header.php');?> 
+    
 </body>
 </html>
---
+++ header.php
@@ -0,0 +1,15 @@
+<header class="header">
+        <img src="https://www.westtown.edu/wp-content/uploads/2023/04/cropped-android-chrome-512x512-1.png" class="logo">
+        <div class="titleContainer">
+            <h1>Opus</h1>
+            <h3>Work Program Management</h3>
+        </div>
+</header>
+<div class="menuBar">
+        <ul>
+            <li><a href="main.php">Schedule</a></li>
+            <li><a href="request-switch.php">Request to Switch</a></li>
+            <li><a href="double-t.php">Double-T</a></li>
+            <li><a href="countdown.php">Countdown</a></li>
+        </ul> 
+</div>
