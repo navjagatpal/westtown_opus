59710a9c2609511c785b715b0965be9e203c4e76
b1dal <125753570+b1dal@users.noreply.github.com>
2024-10-31 09:38:36 -0400

    added green-boxes to navigation bar

---
+++ green-boxes.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opus</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php include('header.php');?> 
+
+    <h1 style="color:white">placeholder - green-boxes.php</h1> <!-- remove this later -->
+    
+</body>
+</html>
